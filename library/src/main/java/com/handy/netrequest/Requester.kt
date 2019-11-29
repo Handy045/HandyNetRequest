3c7594178420a2eaa576bd676a1b2c5e4f850f72
Liu Jie <liujie045.china@outlook.com>
2019-11-29 16:12:17 +0800

    Initial commit

---
+++ library/src/main/java/com/handy/netrequest/Requester.kt
@@ -0,0 +1,18 @@
+package com.handy.netrequest
+
+/**
+ * @title: Requester
+ * @projectName HandyNetRequest
+ * @description: TODO
+ * @author LiuJie https://github.com/Handy045
+ * @date Created in 2019-11-28 17:42
+ */
+class Requester<O> : BaseRequester<O>() {
+    override fun calling(): O {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun analyse(o: O) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ library/src/main/java/com/handy/netrequest/Responder.kt
@@ -0,0 +1,11 @@
+package com.handy.netrequest
+
+/**
+ * @title: Responder
+ * @projectName HandyNetRequest
+ * @description: TODO
+ * @author LiuJie https://github.com/Handy045
+ * @date Created in 2019-11-28 17:43
+ */
+class Responder {
+}
