8eb52e08e99f7df67884adb298cb016270e14186
Liu Jie <liujie045.china@outlook.com>
2019-12-02 16:33:45 +0800

    Andy Commit

---
+++ library/src/main/java/com/handy/netrequest/api/AsyncWithCoroutines.kt
@@ -0,0 +1,42 @@
+package com.handy.netrequest.api
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
+import kotlinx.coroutines.*
+
+/**
+ * @title: AsyncWithCoroutines
+ * @projectName HandyNetRequest
+ * @description: TODO
+ * @author LiuJie https://github.com/Handy045
+ * @date Created in 2019-12-02 16:02
+ */
+
+internal class CoroutineLifecycleListener(private val deferred: Deferred<*>) : LifecycleObserver {
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun cancelCoroutine() {
+        if (!deferred.isCancelled) {
+            deferred.cancel()
+        }
+    }
+}
+
+fun <T> LifecycleOwner.load(loader: suspend () -> T): Deferred<T> {
+    val deferred = GlobalScope.async(context = Dispatchers.Default, start = CoroutineStart.LAZY) {
+        loader()
+    }
+    lifecycle.addObserver(CoroutineLifecycleListener(deferred))
+    return deferred
+}
+
+infix fun <T> Deferred<T>.then(block: suspend (T) -> Unit): Job {
+    return GlobalScope.launch(context = Dispatchers.Main) {
+        try {
+            block(this@then.await())
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
