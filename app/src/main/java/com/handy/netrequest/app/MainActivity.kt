346555ea8629c1bf3b0f8251a60977b281031b51
Liu Jie <liujie045.china@outlook.com>
2019-12-05 09:31:29 +0800

    Andy Commit

--- app/src/main/java/com/handy/netrequest/app/MainActivity.kt
+++ app/src/main/java/com/handy/netrequest/app/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.handy.netrequest.base.BaseResultListener
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    var activity = this
 
     private val handler = Handler()
     private var count = 0
@@ -16,7 +20,11 @@ class MainActivity : AppCompatActivity() {
         override fun run() {
             count++
             Log.i("NetRequest", "$count")
-            handler.postDelayed(this, 1000)
+            if (count < 10) {
+                handler.postDelayed(this, 1000)
+            } else {
+                count = 0
+            }
         }
     }
 
@@ -26,19 +34,60 @@ class MainActivity : AppCompatActivity() {
         btnClick.setOnClickListener {
             handler.post(Counter())
 
-            TestApi(this).setResultListener(object : BaseResultListener<Int>() {
-                override fun onSuccess(data: Int) {
-                    super.onSuccess(data)
-                    Log.d("NetRequest", "onSuccess $data")
-                    Log.d("NetRequest", "onSuccess " + Thread.currentThread().name)
+            // 并发调用
+            GlobalScope.launch(Dispatchers.Main) {
+                val a = async {
+                    TestApi(activity, "a").initialize().await()
+                }
+                val b = async {
+                    TestApi(activity, "b").initialize().await()
+                }
+                val c = async {
+                    TestApi(activity, "c").initialize().await()
+                }
+                val d = async {
+                    TestApi(activity, "d").initialize().await()
+                }
+                val e = async {
+                    TestApi(activity, "e").initialize().await()
+                }
+                val f = async {
+                    TestApi(activity, "f").initialize().await()
+                }
+                val g = async {
+                    TestApi(activity, "g").initialize().await()
                 }
 
-                override fun onFailed(throwable: Throwable) {
-                    super.onFailed(throwable)
-                    Log.d("NetRequest", "onFailed " + throwable.message)
-                    Log.d("NetRequest", "onFailed " + Thread.currentThread().name)
-                }
-            }).initialize().connect()
+                //协程内部是按照代码从上到下的顺序执行，下面的写法并不能达到并发的目的
+//                Log.d("NetRequest", "a is ${a.await()}")
+//                Log.d("NetRequest", "b is ${b.await()}")
+//                Log.d("NetRequest", "c is ${c.await()}")
+//                Log.d("NetRequest", "d is ${d.await()}")
+//                Log.d("NetRequest", "e is ${e.await()}")
+//                Log.d("NetRequest", "f is ${f.await()}")
+//                Log.d("NetRequest", "g is ${g.await()}")
+
+                //同行执行可实现并发效果
+                Log.d(
+                    "NetRequest",
+                    "a is ${a.await()} b is ${b.await()} and c is ${c.await()} and d is ${d.await()} and e is ${e.await()} and f is ${f.await()} and g is ${g.await()}"
+                )
+
+                Log.d("NetRequest", "finish ${Thread.currentThread().name}")
+            }
+
+            // 单独调用
+//            TestApi(this).setResultListener(object : BaseResultListener<String>(null) {
+//                override fun onSuccess(data: String) {
+//                    super.onSuccess(data)
+//                    Log.d("NetRequest", "OnSuccess: ${data}")
+//                }
+//
+//                override fun onFailed(throwable: Throwable) {
+//                    super.onFailed(throwable)
+//                    Log.e("NetRequest", "onFailed: ${throwable.message}")
+//                }
+//            }).initialize().connect()
         }
     }
 }
--- app/src/main/java/com/handy/netrequest/app/TestApi.kt
+++ app/src/main/java/com/handy/netrequest/app/TestApi.kt
@@ -1,8 +1,8 @@
 package com.handy.netrequest.app
 
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.handy.netrequest.base.BaseApiCreater
+import kotlinx.coroutines.delay
 
 /**
  * @title: TestApi
@@ -11,18 +11,22 @@ import com.handy.netrequest.base.BaseApiCreater
  * @author LiuJie https://github.com/Handy045
  * @date Created in 2019-12-03 15:29
  */
-class TestApi(activity: AppCompatActivity) : BaseApiCreater<String, Int>(activity) {
-    override fun call(): String? {
-        Log.d("NetRequest", "call " + Thread.currentThread().name)
-        return "aaaaaaaa"
+class TestApi(activity: AppCompatActivity, tag: String = "TestApi") :
+    BaseApiCreater<Int, String>(activity, tag) {
+    init {
+        isDebug = true
     }
 
-    override fun analyze(response: String): Int? {
-        Log.d("NetRequest", "analyze " + Thread.currentThread().name)
-        if (response != "null") {
-            return response.length
+    override suspend fun call(): Int? {
+        delay(4000L)
+        return 1024
+    }
+
+    override suspend fun analyze(result: Int): String? {
+        if (result == 1024) {
+            return "Hello World"
         } else {
-            errorMessage = "response is null"
+            errorMessage = "返回数据有误"
             return null
         }
     }
