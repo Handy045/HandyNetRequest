3ca8b61a5206d3cbb5f340b465ef7a6dd156b251
Liu Jie <liujie045.china@outlook.com>
2019-12-06 13:35:04 +0800

    Andy Commit

--- app/src/main/java/com/handy/netrequest/app/TestApi.kt
+++ app/src/main/java/com/handy/netrequest/app/TestApi.kt
@@ -3,7 +3,6 @@ package com.handy.netrequest.app
 import androidx.appcompat.app.AppCompatActivity
 import com.handy.netrequest.base.BaseApiCreater
 import com.handy.netrequest.base.BaseResultListener
-import kotlinx.coroutines.delay
 
 /**
  * @title: TestApi
@@ -18,15 +17,13 @@ class TestApi(
     resultListener: BaseResultListener<String>? = null
 ) : BaseApiCreater<Map<String, String>, String>(activity, tag, resultListener) {
 
-    override suspend fun call(): Map<String, String>? {
+    override fun call(): Map<String, String>? {
         //todo 网络请求
-        //模拟请求阻塞
-        delay(3000L)
         //返回请求结果
         return mutableMapOf("flag" to "success", "errorInfo" to "", "data" to "Hello World")
     }
 
-    override suspend fun analyze(result: Map<String, String>): String? {
+    override fun analyze(result: Map<String, String>): String? {
         if (result["flag"] == "success") {
             return result["data"]
         } else {
